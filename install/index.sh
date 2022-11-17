9df4d5b2f67a75809366a8fbb1a397ca46a7deee
Jaakko Leskinen <jaakko.leskinen@gmail.com>
2022-11-17 09:10:08 +0200

    feat: linux/darwin detection and cli tools

--- install/index.sh
+++ install/index.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+# Requires: curl, zsh, unzip
+
+OS=""
+if [ "$(uname)" == "Darwin" ]; then
+    OS="mac"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    OS="linux"
+fi
+
 echo "Installing Oh-My-Zsh..."
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
@@ -14,13 +23,31 @@ curl -L -o $HOME/.alacritty.yml https://raw.githubusercontent.com/jleski/awesome
 echo "Downloading cli binaries to $HOME/bin..."
 mkdir -p $HOME/bin
 
-echo " - kubectl"
-curl -L -o $HOME/bin/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/amd64/kubectl"
-chmod +x $HOME/bin/kubectl
-
-echo " - kubectx and kubens"
-curl -L -o /tmp/go-kubectx.tar.gz https://github.com/aca/go-kubectx/releases/download/v0.1.0/go-kubectx_0.1.0_Darwin_x86_64.tar.gz
-tar xfz /tmp/go-kubectx.tar.gz -C $HOME/bin
-rm /tmp/go-kubectx.tar.gz
+if [ "${OS}" != "" ]; then
+    echo " - kubectl"
+    echo " - kubectx and kubens"
+    echo " - terraform"
+    echo " - lsd"
+    echo " - helm"
+    if [ "${OS}" == "mac" ]; then
+        curl -L -o $HOME/bin/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/amd64/kubectl"
+        curl -L "https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubectx_v0.9.4_darwin_x86_64.tar.gz" | tar -xz -C $HOME/bin kubectx
+        curl -L "https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubens_v0.9.4_darwin_x86_64.tar.gz" | tar -xz -C $HOME/bin kubens
+        curl -L -o "/tmp/terraform.zip" "https://releases.hashicorp.com/terraform/1.3.4/terraform_1.3.4_darwin_amd64.zip" && unzip /tmp/terraform.zip -d $HOME/bin
+        curl -L "https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd-0.23.1-x86_64-apple-darwin.tar.gz" | tar -xz --wildcards --strip-components 1 -C $HOME/bin "*/lsd"
+        curl -L "https://get.helm.sh/helm-v3.10.2-darwin-amd64.tar.gz" | tar -xz -C $HOME/bin helm
+    elif [ "${OS}" == "linux" ]; then
+        curl -L -o $HOME/bin/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+        curl -L "https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubectx_v0.9.4_linux_x86_64.tar.gz" | tar -xz -C $HOME/bin kubectx
+        curl -L "https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubens_v0.9.4_linux_x86_64.tar.gz" | tar -xz -C $HOME/bin kubens
+        curl -L -o "/tmp/terraform.zip" "https://releases.hashicorp.com/terraform/1.3.4/terraform_1.3.4_linux_amd64.zip" && unzip /tmp/terraform.zip -d $HOME/bin
+        curl -L "https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd-0.23.1-aarch64-unknown-linux-gnu.tar.gz" | tar -xz --wildcards --strip-components 1 -C $HOME/bin "*/lsd"
+        curl -L "https://get.helm.sh/helm-v3.10.2-linux-amd64.tar.gz" | tar -xz --wildcards --strip-components 1 -C $HOME/bin "*/helm"
+    fi
+    test -f $HOME/bin/kubectl && chmod +x $HOME/bin/kubectl
+    test -f $HOME/bin/kubectx && chmod +x $HOME/bin/kubectx
+    test -f $HOME/bin/kubens && chmod +x $HOME/bin/kubens
+    test -f $HOME/bin/lsd && chmod +x $HOME/bin/lsd
+fi
 
 echo "Done."
