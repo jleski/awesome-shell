6d08e5d7530c13490d1d6f9de0900e1134855c33
Jaakko Leskinen <jaakko.leskinen@gmail.com>
2022-11-23 13:03:06 +0200

    feat: add kubecm cli

--- install/index.sh
+++ install/index.sh
@@ -36,6 +36,7 @@ if [ "${OS}" != "" ]; then
         curl -L -o "/tmp/terraform.zip" "https://releases.hashicorp.com/terraform/1.3.4/terraform_1.3.4_darwin_amd64.zip" && unzip /tmp/terraform.zip -d $HOME/bin
         curl -L "https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd-0.23.1-x86_64-apple-darwin.tar.gz" | tar -xz --wildcards --strip-components 1 -C $HOME/bin "*/lsd"
         curl -L "https://get.helm.sh/helm-v3.10.2-darwin-amd64.tar.gz" | tar -xz -C $HOME/bin helm
+        curl -L "https://github.com/sunny0826/kubecm/releases/download/v0.21.0/kubecm_v0.21.0_Darwin_x86_64.tar.gz" | tar -xz -C $HOME/bin kubecm
     elif [ "${OS}" == "linux" ]; then
         curl -L -o $HOME/bin/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
         curl -L "https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubectx_v0.9.4_linux_x86_64.tar.gz" | tar -xz -C $HOME/bin kubectx
@@ -43,6 +44,7 @@ if [ "${OS}" != "" ]; then
         curl -L -o "/tmp/terraform.zip" "https://releases.hashicorp.com/terraform/1.3.4/terraform_1.3.4_linux_amd64.zip" && unzip /tmp/terraform.zip -d $HOME/bin
         curl -L "https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd-0.23.1-x86_64-unknown-linux-gnu.tar.gz" | tar -xz --wildcards --strip-components 1 -C $HOME/bin "*/lsd"
         curl -L "https://get.helm.sh/helm-v3.10.2-linux-amd64.tar.gz" | tar -xz --wildcards --strip-components 1 -C $HOME/bin "*/helm"
+        curl -L "https://github.com/sunny0826/kubecm/releases/download/v0.21.0/kubecm_v0.21.0_Linux_x86_64.tar.gz" | tar -xz -C $HOME/bin kubecm
     fi
     test -f $HOME/bin/kubectl && chmod +x $HOME/bin/kubectl
     test -f $HOME/bin/kubectx && chmod +x $HOME/bin/kubectx
