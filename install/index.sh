ada3841787168cdc6787796d8e1a9ad2354607d1
Jaakko Leskinen <jaakko.leskinen@gmail.com>
2023-08-17 10:58:44 +0000

    refactor: install oh-my-zsh after homebrew/utilities

--- install/index.sh
+++ install/index.sh
@@ -10,17 +10,6 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     OS="linux"
 fi
 
-echo "Installing Oh-My-Zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-echo "Fetching dotfiles..."
-
-echo " - zshrc"
-curl -L -o $HOME/.zshrc https://raw.githubusercontent.com/jleski/awesome-shell/main/dotfiles/zshrc
-
-echo " - alacritty"
-curl -L -o $HOME/.alacritty.yml https://raw.githubusercontent.com/jleski/awesome-shell/main/dotfiles/alacritty.yml
-
 echo "Installing applications..."
 mkdir -p $HOME/bin
 
@@ -29,7 +18,7 @@ if [ "${OS}" != "" ]; then
         BREW=$(which brew)
         if [ "${BREW}" != "" ]; then
             echo "Using homebrew..."
-            ${BREW} install kubectl kubectx terraform lsd helm kubecm fzf alacritty rust golang gpg2 gsed az bicep openlens
+            ${BREW} install kubectl kubectx terraform lsd helm kubecm fzf alacritty rust golang gpg2 gsed az bicep openlens zsh
             ${BREW} install --cask visual-studio-code
             ${BREW} tap azure/bicep
             ${BREW} install bicep
@@ -51,7 +40,7 @@ if [ "${OS}" != "" ]; then
         BREW=$(which brew)
         if [ "${BREW}" != "" ]; then
             echo "Using homebrew..."
-            ${BREW} install kubectl kubectx terraform lsd helm kubecm fzf rust golang gpg2 gsed az
+            ${BREW} install kubectl kubectx terraform lsd helm kubecm fzf rust golang gpg2 gsed az zsh
         else
             curl -L -o $HOME/bin/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
             curl -L "https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubectx_v0.9.4_linux_x86_64.tar.gz" | tar -xz -C $HOME/bin kubectx
@@ -68,5 +57,17 @@ if [ "${OS}" != "" ]; then
     test -f $HOME/bin/lsd && chmod +x $HOME/bin/lsd
     test -f $HOME/bin/kubecm && chmod +x $HOME/bin/kubecm
 fi
+
+echo "Installing Oh-My-Zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+echo "Fetching dotfiles..."
+
+echo " - zshrc"
+curl -L -o $HOME/.zshrc https://raw.githubusercontent.com/jleski/awesome-shell/main/dotfiles/zshrc
+
+echo " - alacritty"
+curl -L -o $HOME/.alacritty.yml https://raw.githubusercontent.com/jleski/awesome-shell/main/dotfiles/alacritty.yml
+
 
 echo "Done."
