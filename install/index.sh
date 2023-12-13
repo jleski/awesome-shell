96fe219368a4b1ede05dc59668805c4b5550bb9c
Jaakko Leskinen <jaakko.leskinen@gmail.com>
2023-12-13 12:24:52 +0200

    chore: update mac shell configs

--- install/index.sh
+++ install/index.sh
@@ -16,50 +16,84 @@ mkdir -p $HOME/bin
 if [ "${OS}" != "" ]; then
     if [ "${OS}" == "mac" ]; then
         BREW=$(which brew)
+        if [ "${BREW}" == "" ]; then
+            echo "Setting up homebrew..."
+            NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+            if [ $? -eq 0 ]; then
+                eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+            fi
+            BREW=$(which brew)
+        fi
+
+        echo "Installing Rust ..."
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+        echo "Installing Nerd Fonts..."
+        NERD_FONT_VERSION="3.1.1"
+        curl -L -o "/tmp/FantasqueSansMono.zip" "https://github.com/ryanoasis/nerd-fonts/releases/download/v${NERD_FONT_VERSION}/FantasqueSansMono.zip" && unzip -o /tmp/FantasqueSansMono.zip -d $HOME/Library/Fonts
+
         if [ "${BREW}" != "" ]; then
-            echo "Using homebrew..."
-            ${BREW} install kubectl kubectx terraform lsd helm kubecm fzf alacritty rust golang gpg2 gsed az bicep openlens zsh ansible just go-task
+            echo "Using homebrew to install cmake..."
+            ${BREW} install cmake
+        fi
+
+        echo "Installing Starship ..."
+        $HOME/.cargo/bin/cargo install starship --locked
+
+        GO_VERSION="1.21.5"
+        echo "Installing Go ${GO_VERSION} ..."
+        curl -L -o "/tmp/go${GO_VERSION}.darwin-amd64.pkg" "https://go.dev/dl/go${GO_VERSION}.darwin-amd64.pkg" && sudo installer -pkg "/tmp/go${GO_VERSION}.darwin-amd64.pkg" -target /
+
+        GOOGLE_CLOUD_SDK_VERSION="457.0.0"
+        echo "Installing Google Cloud SDK ${GOOGLE_CLOUD_SDK_VERSION} ..."
+        curl -L "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-${GOOGLE_CLOUD_SDK_VERSION}-darwin-x86_64.tar.gz" | tar -xz -C $HOME
+
+        if [ "${BREW}" != "" ]; then
+            echo "Using homebrew to install apps..."
+            ${BREW} install git kubectl kubectx terraform lsd helm kubecm fzf alacritty gpg2 gsed az openlens zsh ansible just go-task direnv
+            ${BREW} install cloudflare/cloudflare/cloudflared
             ${BREW} install --cask visual-studio-code
+            ${BREW} install --cask vscodium
             ${BREW} tap azure/bicep
             ${BREW} install bicep
             ${BREW} install --cask powershell
             pwsh -Command "Install-Module -Name Az -Repository PSGallery -Force"
         else
-            echo "Downloading cli binaries to $HOME/bin..."
-            curl -L -o $HOME/bin/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/darwin/amd64/kubectl"
-            curl -L "https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubectx_v0.9.4_darwin_x86_64.tar.gz" | tar -xz -C $HOME/bin kubectx
-            curl -L "https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubens_v0.9.4_darwin_x86_64.tar.gz" | tar -xz -C $HOME/bin kubens
-            curl -L -o "/tmp/terraform.zip" "https://releases.hashicorp.com/terraform/1.3.4/terraform_1.3.4_darwin_amd64.zip" && unzip /tmp/terraform.zip -d $HOME/bin
-            curl -L "https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd-0.23.1-x86_64-apple-darwin.tar.gz" | tar -xz --wildcards --strip-components 1 -C $HOME/bin "*/lsd"
-            curl -L "https://get.helm.sh/helm-v3.10.2-darwin-amd64.tar.gz" | tar -xz -C $HOME/bin helm
-            curl -L "https://github.com/sunny0826/kubecm/releases/download/v0.21.0/kubecm_v0.21.0_Darwin_x86_64.tar.gz" | tar -xz -C $HOME/bin kubecm
+            echo "Warning: Homebrew not found on path. Apps may not be installed."
         fi
+
+        echo "Configuring 1Password SSH Agent..."
+        export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
+
     elif [ "${OS}" == "linux" ]; then
         NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
         if [ $? -eq 0 ]; then
             eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
         fi
         BREW=$(which brew)
+        echo "Installing Rust ..."
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+        if [ "${BREW}" != "" ]; then
+            echo "Using homebrew to install cmake..."
+            ${BREW} install cmake
+        fi
+
+        echo "Installing Starship ..."
+        $HOME/.cargo/bin/cargo install starship --locked
+
         if [ "${BREW}" != "" ]; then
             echo "Using homebrew..."
-            ${BREW} install kubectl kubectx terraform lsd helm kubecm fzf rust golang gpg2 gsed az zsh ansible just go-task
+            ${BREW} install kubectl kubectx terraform lsd helm kubecm fzf golang gpg2 gsed az zsh ansible just go-task direnv
+            ${BREW} install cloudflare/cloudflare/cloudflared
+            ${BREW} tap azure/bicep
+            ${BREW} install bicep
             ${BREW} install --cask powershell
             pwsh -Command "Install-Module -Name Az -Repository PSGallery -Force"
         else
-            curl -L -o $HOME/bin/kubectl "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-            curl -L "https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubectx_v0.9.4_linux_x86_64.tar.gz" | tar -xz -C $HOME/bin kubectx
-            curl -L "https://github.com/ahmetb/kubectx/releases/download/v0.9.4/kubens_v0.9.4_linux_x86_64.tar.gz" | tar -xz -C $HOME/bin kubens
-            curl -L -o "/tmp/terraform.zip" "https://releases.hashicorp.com/terraform/1.3.4/terraform_1.3.4_linux_amd64.zip" && unzip /tmp/terraform.zip -d $HOME/bin
-            curl -L "https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd-0.23.1-x86_64-unknown-linux-gnu.tar.gz" | tar -xz --wildcards --strip-components 1 -C $HOME/bin "*/lsd"
-            curl -L "https://get.helm.sh/helm-v3.10.2-linux-amd64.tar.gz" | tar -xz --wildcards --strip-components 1 -C $HOME/bin "*/helm"
-            curl -L "https://github.com/sunny0826/kubecm/releases/download/v0.21.0/kubecm_v0.21.0_Linux_x86_64.tar.gz" | tar -xz -C $HOME/bin kubecm
+            echo "Warning: Homebrew not found on path. Apps may not be installed."
         fi
     fi
-    test -f $HOME/bin/kubectl && chmod +x $HOME/bin/kubectl
-    test -f $HOME/bin/kubectx && chmod +x $HOME/bin/kubectx
-    test -f $HOME/bin/kubens && chmod +x $HOME/bin/kubens
-    test -f $HOME/bin/lsd && chmod +x $HOME/bin/lsd
-    test -f $HOME/bin/kubecm && chmod +x $HOME/bin/kubecm
 fi
 
 echo "Installing Oh-My-Zsh..."
@@ -67,11 +101,19 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 echo "Fetching dotfiles..."
 
-echo " - zshrc"
-curl -L -o $HOME/.zshrc https://raw.githubusercontent.com/jleski/awesome-shell/main/dotfiles/zshrc
-
+if [ "${OS}" != "" ]; then
+    if [ "${OS}" == "mac" ]; then
+        echo " - zshrc (mac)"
+        curl -L -o $HOME/.zshrc https://sh.jles.work/dotfiles/zshrc_mac
+    elif [ "${OS}" == "linux" ]; then
+        echo " - zshrc (linux)"
+        curl -L -o $HOME/.zshrc https://sh.jles.work/dotfiles/zshrc_linux
+    fi
+fi
 echo " - alacritty"
-curl -L -o $HOME/.alacritty.yml https://raw.githubusercontent.com/jleski/awesome-shell/main/dotfiles/alacritty.yml
+curl -L -o $HOME/.alacritty.yml https://sh.jles.work/dotfiles/alacritty.yml
 
+echo "Configuring Starship preset..."
+$HOME/.cargo/bin/starship preset tokyo-night -o ~/.config/starship.toml
 
 echo "Done."
